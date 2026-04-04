7a13bb6ed0618ced59d70d4f83ebf2f8aa4335ff
VladSebov <sebov.vlad@inbox.ru>
2026-04-04 20:39:26 +0300

    feat: добавить ValidationPipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
 
   const port = configService.get<number>('NEST_API_PORT') || 3000;
   const vueAppPort = configService.get<number>('VUE_APP_PORT') || 9000;
