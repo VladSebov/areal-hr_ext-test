7ec28bed3d0c6ac4b12f46ef5d2b92f65c242f82
VladSebov <sebov.vlad@inbox.ru>
2026-04-04 20:55:07 +0300

    feat: добавить валидацию данных

--- api/src/departments/dto/create-department.dto.ts
+++ api/src/departments/dto/create-department.dto.ts
@@ -1,6 +1,22 @@
+import { IsString, IsNotEmpty, IsOptional, IsInt, MinLength, MaxLength } from 'class-validator';
+
 export class CreateDepartmentDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(100)
     name: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(500)
     comment?: string;
+
+    @IsInt()
+    @IsNotEmpty()
     organizationId: number;
+
+    @IsOptional()
+    @IsInt()
     parentId?: number;
 }
