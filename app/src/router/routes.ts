3116aab6857a501eb719b44d2cd1677ed4141460
VladSebov <sebov.vlad@inbox.ru>
2026-04-21 12:29:22 +0300

    feat: добавить vue-страницу для управления пользователями

--- app/src/router/routes.ts
+++ app/src/router/routes.ts
@@ -10,6 +10,7 @@ const routes: RouteRecordRaw[] = [
       { path: 'departments', component: () => import('pages/DepartmentsPage.vue') },
       { path: 'positions', component: () => import('pages/PositionsPage.vue') },
       { path: 'employees', component: () => import('pages/EmployeesPage.vue') },
+      { path: 'users', component: () => import('pages/UsersPage.vue') },
       { path: 'files', component: () => import('pages/FilesPage.vue') },
       { path: 'hr-operations', component: () => import('pages/HrOperationsPage.vue') },
       { path: 'operations-history', component: () => import('pages/OperationsHistoryPage.vue') }
