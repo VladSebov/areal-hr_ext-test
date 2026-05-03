8722c277a4046b8647bf0ff7400fd9caf46c9628
VladSebov <sebov.vlad@inbox.ru>
2026-05-03 13:56:12 +0300

    fix(auth): выдавать исключение unauthorized

--- api/src/auth/guards/authenticated.guard.ts
+++ api/src/auth/guards/authenticated.guard.ts
@@ -1,9 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
+
+    if (!request.isAuthenticated()) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    return true;
   }
 }
